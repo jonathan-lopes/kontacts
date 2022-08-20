883c4544d945fad114c0ffbb4c8d85d8f6fcf01f
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-20 00:42:09 -0300

    project migrated to public

---
+++ README.md
@@ -0,0 +1,20 @@
+![](https://i.imgur.com/xG74tOh.png)
+
+# Aula prática de React
+
+## Agenda de contatos pessoal
+
+O objetivo dessa aula é você desenvolver uma aplicação do começo ao fim, utilizando as tecnologias aprendidas durante o curso.
+
+A aplicação trata-se de uma agenda pessoal, onde você pode fazer um cadastro de e autenticar-se. Após se autenticar, o usuário pode adicionar, editar, remover e listar contatos em sua agenda.
+
+Para desenvolver, nesse repositório temos um arquivo figma chamado `design.fig`.
+
+Você pode ver e testar a aplicação nesse link => [https://kontacts.netlify.app/](https://cubosagenda.netlify.app/)
+
+
+Para acessar a API [clique aqui](https://github.com/jonathan-lopes/api-kontacts)
+
+
+
+###### tags: `módulo 4` `front-end` `React`
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import SignUpBackground from "../../assets/sign-up-background.jpg";
+import toast from "../../helpers/toast";
+import useRequests from "../../hooks/useRequests";
+import "./styles.css";
+
+const defaultValuesForm = { name: "", email: "", password: "" };
+
+function SignUp() {
+  const [form, setForm] = useState(defaultValuesForm);
+  const history = useHistory();
+  const requests = useRequests();
+
+  function handleChange(target) {
+    setForm({
+      ...form,
+      [target.name]: target.value,
+    });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!form.email || !form.password || !form.name) {
+      return;
+    }
+
+    const body = {
+      name: form.name,
+      email: form.email,
+      passwd: form.password,
+    };
+
+    const result = await requests.post("usuarios", body);
+
+    console.log("result:", result);
+
+    if (result) {
+      toast.messageSuccess("Usuário cadastrado com sucesso");
+      history.push("/");
+    }
+  }
+
+  return (
+    <main>
+      <div className="container-left">
+        <form onSubmit={handleSubmit}>
+          <h2>Cadastre-se</h2>
+          <input
+            name="name"
+            type="text"
+            placeholder="Nome"
+            value={form.name}
+            onChange={(e) => handleChange(e.target)}
+          />
+
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            value={form.email}
+            onChange={(e) => handleChange(e.target)}
+          />
+
+          <input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            value={form.password}
+            onChange={(e) => handleChange(e.target)}
+          />
+
+          <button className="btn btn-positive btn-register">CADASTRAR</button>
+          <button className="btn btn-negative btn-cancel">CANCELAR</button>
+        </form>
+
+        <div className="login-link">
+          <span>Já tem cadastro?</span>&nbsp;
+          <Link to="/sign-in">Clique aqui!</Link>
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(${SignUpBackground})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      ></div>
+    </main>
+  );
+}
+
+export default SignUp;
